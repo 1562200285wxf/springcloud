3ff2997191616118fa84136e3fa50c3ff7f305d7
wangxiaofeng <1562200285@qq.com>
2021-11-22 11:09:04 +0800

    mapper代码初始化

---
+++ spring-acl/src/main/java/com/bh/mapper/RolePermissionMapper.java
@@ -0,0 +1,17 @@
+package com.bh.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.bh.entity.RolePermission;
+
+/**
+ * <p>
+ * 角色权限 Mapper 接口
+ * </p>
+ *
+ * @Author: wangxiaofeng
+ * @DateTime: 2021/11/22 11:07
+ * @Description: TODO
+ */
+public interface RolePermissionMapper extends BaseMapper<RolePermission> {
+
+}
---
+++ spring-acl/src/main/java/com/bh/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.bh.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.bh.entity.User;
+
+/**
+ * <p>
+ * 用户表 Mapper 接口
+ * </p>
+ *
+ * @Author: wangxiaofeng
+ * @DateTime: 2021/11/22 11:07
+ * @Description: TODO
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
